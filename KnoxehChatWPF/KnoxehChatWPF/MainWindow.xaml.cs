0aecff2e0ba7abaa0669c825bbe66a0021ff1b2b
Knoxeh <nolan@knoxeh.com>
2014-03-14 15:24:31 -0400

    Initial Commit
    +Chat Client (KnoxehChatWPF)
    + Chat Server (TCPServerCSharp)

---
+++ KnoxehChatWPF/KnoxehChatWPF/MainWindow.xaml.cs
@@ -0,0 +1,340 @@
+﻿using Awesomium.Core;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Media;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace KnoxehChatWPF
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        String serveIP = "murphy.im";
+        SoundPlayer notificationSound = new SoundPlayer(@"./Mira.wav");
+               
+        private static NetworkStream stream;
+        private static TcpClient client;
+        private static Boolean serverConnected = false;
+        private static String userName = "Guest";
+
+        List<chatMessage> chatLog = new List<chatMessage>();
+        public static int chatAmount = 0;
+        Thread inputThread;
+
+       public delegate void updateChatBoxDelegate(String textBoxString); // delegate type 
+        public static updateChatBoxDelegate updateChatBox; // delegate object
+        void updateChatBox1(string str) { createChatMessage(userName, str); }
+        
+        public MainWindow()
+        {
+            InitializeComponent();
+
+  
+            
+        }
+        private void sendClientStatus(String status)
+        {
+
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("STATUS:" + status);
+            stream.Write(data, 0, data.Length);
+
+        }
+        private void sendChatMessage(String message)
+        {
+            if (serverConnected)
+            {
+                message = message.Replace(":", "&#58;");
+                string msgSend = "MSG:" + userName + ":" + message + "";
+                Byte[] data = System.Text.Encoding.ASCII.GetBytes(msgSend.Length+":"+msgSend);
+                stream.Write(data, 0, data.Length);
+                Debug.WriteLine("SENDING MESSAGE: " + message);
+            }
+        }
+        private String getServerInfo(String type)
+        {
+            Byte[] data = System.Text.Encoding.ASCII.GetBytes("GETDATA:" + type);
+            stream.Write(data, 0, data.Length);
+
+            Int32 bytes = stream.Read(data, 0, data.Length);
+            String responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+
+            return responseData;
+        }
+        public void handleChatMessage(string[] msg)
+        {
+
+            this.Dispatcher.Invoke((Action)(() =>
+            {
+             
+                if(msg[1] == "MSG")
+                {
+                    createChatMessage(msg[2], msg[3]);
+                }
+                else if(msg[0] == "SRVMSG")
+                {
+                    if (msg[1] == "userConnect")
+                    {
+                        createServerMessage(msg[2] + " has connected");
+                        if(msg[2] != userName)
+                            userList.Items.Add(msg[2]);
+                    }
+                    else if(msg[1] == "userDisconnect")
+                    {
+                        createServerMessage(msg[2] + " has disconnected");
+                        userList.Items.Remove(msg[2]);
+                       
+                    }
+                    else if(msg[1] == "ChangeName")
+                    {
+                        int userLocation = userList.Items.IndexOf(msg[2]);
+                        createServerMessage(msg[2] + " has changed names to " + msg[3]);
+                        userList.Items.RemoveAt(userLocation);
+                        userList.Items.Insert(userLocation, msg[3]);
+                    }
+                    else if(msg[1] == "userList")
+                    {
+                        String[] userListSplit = msg[2].Split(';');
+                        foreach (String users in userListSplit)
+                            userList.Items.Add(users);
+
+                    }
+                    
+                }
+              
+            }));
+        }
+        public void createServerMessage(string msg)
+        {
+            chatLog.Add(new chatMessage("server","SERVER",msg));
+            string json = JsonConvert.SerializeObject(chatLog, Formatting.None);
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("chatLog.json");
+            file.Write(json);
+            file.Close();
+
+            txtChat.AppendText("SERVER: " + msg + Environment.NewLine);
+        }
+        public void createChatMessage(string author, string message)
+        {
+            chatLog.Add(new chatMessage("user", author, message));
+
+            string json = JsonConvert.SerializeObject(chatLog, Formatting.None);
+
+            if(chkSound.IsChecked == true)
+                  notificationSound.Play();
+
+            System.IO.StreamWriter file = new System.IO.StreamWriter("chatLog.json");
+            file.Write(json);
+            file.Close();
+
+            txtChat.AppendText(author + ": " + message + Environment.NewLine);
+
+        }
+        private void btnSend_Click(object sender, RoutedEventArgs e)
+        {
+            String messageInput = txtMessage.Text;
+
+            //Handle slash commands
+            if (messageInput.Contains("/name"))
+            {
+                String newName = messageInput.Substring(6);
+                sendClientStatus("ChangeName:" + userName + ":" + newName);
+                userName = newName;
+                txtUserName.Text = userName;
+            }
+
+            else//If there arent any slash commands
+            {
+                sendChatMessage(messageInput );
+            }
+            txtMessage.Text = "";
+           
+        }
+        public void HandleUserList()
+        {
+            while(serverConnected)
+            {
+                Thread.Sleep(1000);
+                
+
+
+            }
+            
+        }
+        public void HandleMessages()
+        {
+            Byte[] data = new Byte[1024];
+            String responseData = String.Empty;
+            Thread.Sleep(10);
+
+            while (serverConnected)
+            {
+                Thread.Sleep(100);
+               
+                Int32 bytes = stream.Read(data, 0, data.Length);
+                responseData = System.Text.Encoding.ASCII.GetString(data, 0, bytes);
+                
+
+                string[] message = responseData.Split(':');
+                handleChatMessage(message);
+
+            }
+        }
+
+        private void btnSend_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void btnConnect_Click(object sender, RoutedEventArgs e)
+        {
+            btnConnect.IsEnabled = false;
+            try
+            {
+                serverConnected = !serverConnected;
+                if (serverConnected)
+                {
+                    Int32 port = 13000;
+                    client = new TcpClient(serveIP, port);
+                    stream = client.GetStream();
+                    btnConnect.Content = "Disconnect from Server";
+                    userName = txtUserName.Text;
+                    sendClientStatus("onConnect:" + userName);
+
+                    // client started
+                    // create a thread to handle communication
+                    inputThread = new Thread(new ThreadStart(HandleMessages));
+                    inputThread.Start();
+
+
+                    txtUserName.IsEnabled = false;
+
+                    webChat.Source = new Uri("file:///./chatHandle.html",UriKind.Absolute);
+
+                    Thread.Sleep(500);
+                    btnConnect.IsEnabled = true;
+
+                }
+                else
+                {
+                    userList.Items.Clear();
+                    txtUserName.IsEnabled = true;
+                    btnConnect.Content = "Connect To Server";
+                    sendClientStatus("onDisconnect:" + userName);
+                    inputThread.Abort();
+                    stream.Flush();
+                    stream.Close();
+                    client.Close();
+
+                    Thread.Sleep(500);
+                    btnConnect.IsEnabled = true;
+                    //   timer1.Stop();
+                }
+            }
+            catch(Exception ex)
+            {
+                if (ex.GetType().ToString() == "System.Net.Sockets.SocketException")
+                {
+                    MessageBox.Show(serveIP + " is not responding, please try connecting again later!","Connection Error");
+                }
+
+                
+            }
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            if(serverConnected)
+            {
+
+                btnConnect_Click(sender,null);
+            }
+        }
+
+        private void txtMessage_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Return)
+            {
+                btnSend_Click(sender, e);
+            }
+            
+        }
+
+        private void webChat_LoadingFrame(object sender, Awesomium.Core.LoadingFrameEventArgs e)
+        {
+
+           
+        }
+        private void webChat_ShowJavascriptDialog(object sender, Awesomium.Core.JavascriptDialogEventArgs e)
+        {
+            
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            
+        }
+
+        private void webChat_Unloaded(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void webChat_InitializeView(object sender, WebViewEventArgs e)
+        {
+            // Create a WebSession.
+            WebSession session = WebCore.CreateWebSession(new WebPreferences()
+            {
+                SmoothScrolling = true,
+                UniversalAccessFromFileURL = true
+            });
+            
+
+            // Assign it to the control. This should only occur here, before the
+            // the underlying view of the control is created.
+            webChat.WebSession = session;
+        }
+    }
+    class chatMessage
+    {
+        public string timestamp;
+        public int id;
+        public String messageType;
+        public String author;
+        public String message;
+        public String messageHTML;
+        public chatMessage( String type, String athr, String msg)
+        {
+            timestamp = DateTime.Now.ToString("MMM ddd d HH:mm yyyy");
+            messageType = type;
+            author = athr;
+            message = msg;
+            MainWindow.chatAmount++;
+            id = MainWindow.chatAmount;
+            switch(messageType)
+            {
+                case "server": messageHTML = "<div class='row'><div class='col-md-12'><div class='alert alert-success'><b>SERVER:</b> " + message + "</div></div></div>"; break;
+                case "user": messageHTML = "<div class=\"row\"><div class=\"col-md-1\"><strong>" + author + "</strong></div><div class=\"col-md-11 well well-sm\">" + message + "</div></div>"; break;
+            }
+            
+        }
+    }
+}
---
+++ TCPServerCSharp/TCPServerCSharp/Program.cs
@@ -0,0 +1,218 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace TCPServerCSharp
+{
+    class TcpServ
+    {
+        private TcpListener _server;
+        private int userCount;
+        
+        private Boolean _isRunning;
+        private Boolean clientConnected;
+        private List<String> userList = new List<String>();
+        private List<String> ChatLog = new List<String>();
+
+        public TcpServ(int port)
+        {
+            _server = new TcpListener(IPAddress.Any, port);
+            _server.Start();
+
+            _isRunning = true;
+
+            LoopClients();
+
+
+        }
+
+        public void LoopClients()
+        {
+            while (_isRunning)
+            {
+                // wait for client connection
+                TcpClient newClient = _server.AcceptTcpClient();
+                userCount++;
+                // client found.
+                // create a thread to handle communication
+                Thread t = new Thread(new ParameterizedThreadStart(HandleClient));
+                t.Start(newClient);
+            }
+        }
+        public void HandleMessages(object obj)
+        {
+            TcpClient client = (TcpClient)obj;
+
+            NetworkStream sStream = client.GetStream();
+
+            int ChatLogSize = ChatLog.Count;
+            int userListSize = userList.Count;
+            while (clientConnected)
+            {
+                Thread.Sleep(100);
+                if (ChatLog.Count > ChatLogSize && clientConnected)
+                {
+                    byte[] msg1 = System.Text.Encoding.ASCII.GetBytes(ChatLog[ChatLog.Count - 1]);
+                    sStream.Write(msg1, 0, msg1.Length);
+                    Console.WriteLine("Sending Message: {0}",ChatLog[ChatLog.Count - 1]);
+                    ChatLogSize = ChatLog.Count;
+                }
+                /*if(userList.Count > ChatLogSize && clientConnected)
+                {
+                    String userListStr = "";
+                    for (int x = 0; x < userList.Count; x++)
+                    {
+                        userListStr += userList[x];
+                        if (x != (userCount - 1))
+                            userListStr += ";";
+                    }
+                    byte[] msg = System.Text.Encoding.ASCII.GetBytes("SRVSTATUS:USERLIST:" + userListStr);
+                    sStream.Write(msg, 0, msg.Length);
+                    userListSize = userList.Count;
+                }*/
+            }
+
+        }
+        public void HandleClient(object obj)
+        {
+            // retrieve client from parameter passed to thread
+            TcpClient client = (TcpClient)obj;
+            string clientIPAddress = "" + IPAddress.Parse(((IPEndPoint)client.Client.RemoteEndPoint).Address.ToString());
+            Console.WriteLine("NEW CLIENT: {0}", clientIPAddress);
+
+            // sets two streams
+            //StreamWriter sWriter = new StreamWriter(client.GetStream(), Encoding.ASCII);
+           // StreamReader sReader = new StreamReader(client.GetStream(), Encoding.ASCII);
+            NetworkStream sStream = client.GetStream();
+
+            // you could use the NetworkStream to read and write, 
+            // but there is no forcing flush, even when requested
+            clientConnected = true;
+            Byte[] bytes = new Byte[1024];
+            String data = null;
+            int i;
+            
+
+            Thread t = new Thread(new ParameterizedThreadStart(HandleMessages));
+            t.Start(client);
+            while (clientConnected)
+            {
+                try
+                { 
+
+                    // Loop to receive all the data sent by the client. 
+                    while ((i = sStream.Read(bytes, 0, bytes.Length)) != 0)
+                    {
+
+                        Thread.Sleep(100);
+                        // Translate data bytes to a ASCII string.
+                        data = System.Text.Encoding.ASCII.GetString(bytes, 0, i);
+                        Console.WriteLine("Received: {0}", data);
+
+                        // Process the data sent by the client.
+                        //data = data.ToUpper();
+                       
+                        byte[] msg = System.Text.Encoding.ASCII.GetBytes(data);
+                        string[] command = data.Split(':');
+
+                       
+                        if(command[1] == "MSG")
+                        {
+                            if(data.Substring(0,1) != "/")
+                                ChatLog.Add(data);
+                        }
+                        else if(command[0] == "STATUS")
+                        {
+                            Thread.Sleep(10);
+                            if(command[1] == "ChangeName")
+                            {
+
+                                ChatLog.Add("SRVMSG:ChangeName:"+command[2]+":"+command[3]);
+                                userList[userList.IndexOf(command[2])] = command[3];
+                            }
+                            else if(command[1] == "onDisconnect")
+                            {
+                                ChatLog.Add("SRVMSG:userDisconnect:"+command[2]);
+
+                                int userIndex = userList.IndexOf(command[2]);
+                                userList.RemoveAt(userIndex);
+                                Console.WriteLine("Removing User: {0}",userIndex);
+                                clientConnected = false;
+                               // sStream.
+                            }
+                            else if(command[1] == "onConnect")      
+                            {
+                                userList.Add(command[2]);
+                                ChatLog.Add("SRVMSG:userConnect:"+command[2]);
+
+                                String userListStr = "";
+                                for (int x = 0; x < userList.Count; x++)
+                                {
+                                    userListStr += userList[x];
+                                    if (x != (userCount - 1))
+                                        userListStr += ";";
+                                }
+                                msg = System.Text.Encoding.ASCII.GetBytes("SRVMSG:userList:" + userListStr);
+                                sStream.Write(msg, 0, msg.Length);
+                            }
+                            
+
+                        
+                        }
+                        else if(command[0] == "GETDATA")
+                        {
+                            if(command[1] == "userList")
+                            {
+                                String userListStr = "";
+                                for (int x = 0; x < userList.Count; x++)
+                                {
+                                    userListStr += userList[x];
+                                    if (x != (userCount - 1))
+                                        userListStr += ";";
+                                }
+                                msg = System.Text.Encoding.ASCII.GetBytes("SRVMSG:userList:"+userListStr);
+                                sStream.Write(msg, 0, msg.Length);
+                                Console.WriteLine("Sent: {0}", userListStr);
+                                
+                            }
+                        }
+                        else if(data == "GETDATA:USERCOUNT")
+                        {
+                            msg = System.Text.Encoding.ASCII.GetBytes(""+userCount);
+                            sStream.Write(msg, 0, msg.Length);
+                            Console.WriteLine("Sent: {0}", userCount);
+                        }
+                        
+                        // Send back a response.
+                        //sStream.Write(msg, 0, msg.Length);
+                        //Console.WriteLine("Sent: {0}", data);
+
+
+                        
+                    }
+                }
+                catch (IOException exception)
+                {
+                    userCount--;
+                    Console.WriteLine("Client Forcefully Close Stream.");
+                    clientConnected = false;
+                }            
+            }
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            TcpServ ts = new TcpServ(13000);
+        }
+    }
+}
+
